c9b13cff59429fd66a6e185dfb850fd0d6183173
Paul Noel <panoel007@gmail.com>
2024-09-18 16:37:13 -0500

    evm: add refund address

--- evm/src/interfaces/IRouter.sol
+++ evm/src/interfaces/IRouter.sol
@@ -7,11 +7,14 @@ import "../libraries/UniversalAddress.sol";
 interface IRouter is IMessageSequence {
     /// @dev Send a message to another chain.
     /// @param recipientChain The Wormhole chain ID of the recipient.
-    /// @param recipientAddress The Wormhole formatted address of the peer NTT Manager on the recipient chain.
+    /// @param recipientAddress The universal address of the peer NTT Manager on the recipient chain.
+    /// @param refundAddress The address to refund the message fee to.
     /// @param message A message to be sent to the nttManager on the recipient chain.
     /// @return uint64 The sequence number of the message.
-    function sendMessage(uint16 recipientChain, UniversalAddress recipientAddress, bytes memory message)
-        external
-        payable
-        returns (uint64);
+    function sendMessage(
+        uint16 recipientChain,
+        UniversalAddress recipientAddress,
+        UniversalAddress refundAddress,
+        bytes memory message
+    ) external payable returns (uint64);
 }
