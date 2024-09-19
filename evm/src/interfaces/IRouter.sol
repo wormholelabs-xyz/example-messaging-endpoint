a839ac5552c8e8862022ba31e95c1581fd75cfd1
Paul Noel <panoel007@gmail.com>
2024-09-18 19:42:14 -0500

    evm: change address

--- evm/src/interfaces/IRouter.sol
+++ evm/src/interfaces/IRouter.sol
@@ -7,14 +7,14 @@ import "../libraries/UniversalAddress.sol";
 interface IRouter is IMessageSequence {
     /// @dev Send a message to another chain.
     /// @param recipientChain The Wormhole chain ID of the recipient.
-    /// @param recipientAddress The universal address of the peer NTT Manager on the recipient chain.
-    /// @param refundAddress The address to refund the message fee to.
-    /// @param message A message to be sent to the nttManager on the recipient chain.
+    /// @param recipientAddress The universal address of the peer on the recipient chain.
+    /// @param refundAddress The source chain address to refund the message fee to.
+    /// @param message A message to be sent to the recipient chain.
     /// @return uint64 The sequence number of the message.
     function sendMessage(
         uint16 recipientChain,
         UniversalAddress recipientAddress,
-        UniversalAddress refundAddress,
+        address refundAddress,
         bytes memory message
     ) external payable returns (uint64);
 }
