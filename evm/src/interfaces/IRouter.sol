e106f21eadcd46d2dc7a2e145a9508cda5f4bdf8
Paul Noel <panoel007@gmail.com>
2024-09-19 08:12:26 -0500

    evm: update comment

--- evm/src/interfaces/IRouter.sol
+++ evm/src/interfaces/IRouter.sol
@@ -8,7 +8,7 @@ interface IRouter is IMessageSequence {
     /// @dev Send a message to another chain.
     /// @param recipientChain The Wormhole chain ID of the recipient.
     /// @param recipientAddress The universal address of the peer on the recipient chain.
-    /// @param refundAddress The source chain address to refund the message fee to.
+    /// @param refundAddress The source chain refund address passed to the Transceiver.
     /// @param message A message to be sent to the recipient chain.
     /// @return uint64 The sequence number of the message.
     function sendMessage(
