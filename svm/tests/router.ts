9393c94844d4ee6900ce0cc2e6dbcd66cfa08c99
bingyuyap <bingyu.yap.21@gmail.com>
2024-09-25 23:28:22 +0800

    solana: fix ci
    Signed-off-by: bingyuyap <bingyu.yap.21@gmail.com>

--- svm/tests/router.ts
+++ svm/tests/router.ts
@@ -1,16 +1,17 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { Svm } from "../target/types/svm";
+import { Router } from "../target/types/router";
 
-describe("svm", () => {
+describe("router", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.Svm as Program<Svm>;
+  const program = anchor.workspace.Router as Program<Router>;
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const owner = anchor.web3.Keypair.generate();
+    const tx = await program.methods.initialize({ owner: owner.publicKey }).rpc();
     console.log("Your transaction signature", tx);
   });
 });
