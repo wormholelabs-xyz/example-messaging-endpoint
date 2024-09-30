ee9aeeb7f23193c19e63330033936ca71d859f83
bingyuyap <bingyu.yap.21@gmail.com>
2024-09-30 20:47:43 +0800

    solana: add missing DERIVE_SPACE
    Signed-off-by: bingyuyap <bingyu.yap.21@gmail.com>

--- svm/programs/router/src/state/integrator_chain_transceivers.rs
+++ svm/programs/router/src/state/integrator_chain_transceivers.rs
@@ -1,16 +1,16 @@
 use anchor_lang::prelude::*;
 
 #[account]
+#[derive(InitSpace)]
 pub struct IntegratorChainTransceivers {
     pub bump: u8,
     pub integrator_id: u64,
     pub chain_id: u16,
     pub next_transceiver_id: u64,
-    pub transceiver_bitmap: u64, // support up to 64 transceivers
+    pub transceiver_bitmap: u64,
 }
 
 impl IntegratorChainTransceivers {
     pub const SEED_PREFIX: &'static [u8] = b"integrator_chain_transceivers";
-    pub const INIT_SPACE: usize = 8 + 1 + 8 + 8 + 8 + 8; // 8 (discriminator) + 1 + 8 + 8 + 8 + 8 bytes
     pub const MAX_TRANSCEIVERS: usize = 64;
 }
