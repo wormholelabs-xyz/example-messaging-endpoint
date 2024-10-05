0de6c77488f2c060483d97a3becf8eed1ddada4b
bingyuyap <bingyu.yap.21@gmail.com>
2024-10-05 10:18:08 +0800

    solana: remove owner and paused
    Signed-off-by: bingyuyap <bingyu.yap.21@gmail.com>

--- svm/programs/router/src/state/config.rs
+++ svm/programs/router/src/state/config.rs
@@ -3,7 +3,6 @@ use anchor_lang::prelude::*;
 /// Represents the global configuration of the GMP Router.
 ///
 /// This struct stores important global state including:
-/// - The program owner's public key
 /// - Whether the program is paused
 /// - A counter for assigning unique IDs to integrators
 ///
@@ -13,12 +12,6 @@ use anchor_lang::prelude::*;
 pub struct Config {
     /// Bump seed for PDA derivation
     pub bump: u8,
-
-    /// Public key of the program owner
-    pub owner: Pubkey,
-
-    /// Flag to pause/unpause the program
-    pub paused: bool,
 
     /// Counter for assigning unique IDs to integrators
     pub next_integrator_id: u64,
