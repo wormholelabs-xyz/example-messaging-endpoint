901d1ff8a019f58653dde138799ef9135fdaea49
bingyuyap <bingyu.yap.21@gmail.com>
2024-10-08 17:11:41 +0800

    solana: register_transceiver
    Signed-off-by: bingyuyap <bingyu.yap.21@gmail.com>

--- svm/programs/router/src/instructions/mod.rs
+++ svm/programs/router/src/instructions/mod.rs
@@ -2,10 +2,8 @@ pub mod initialize;
 pub mod initialize_integrator_chain_transceivers;
 pub mod initialize_integrator_config;
 pub mod register_transceiver;
-pub mod transfer_integrator_chain_transceivers_ownership;
 
 pub use initialize::*;
 pub use initialize_integrator_chain_transceivers::*;
 pub use initialize_integrator_config::*;
 pub use register_transceiver::*;
-pub use transfer_integrator_chain_transceivers_ownership::*;
